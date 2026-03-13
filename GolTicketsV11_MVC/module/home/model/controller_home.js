c45f31cbabcdbda3b37ac07dd9e79b3c1175e9c9
nando <fernandogmfp@gmail.com>
2026-03-13 12:54:52 +0100

    V11 home con carrusel

---
+++ GolTicketsV11_MVC/module/home/model/controller_home.js
@@ -0,0 +1,134 @@
+function loadCarousel() {
+    ajaxPromise('module/home/controller/controller_home.php?op=homePageCarousel', 'GET', 'JSON')
+    .then(function(data) {
+        for (let row = 0; row < data.length; row++) {
+
+            $('<div></div>')
+                .addClass('swiper-slide partido-slide')
+                .appendTo('#containerCarousel')
+                .html(
+                    '<div class="partido-slide-inner" style="background-image: url(\'' + data[row].img_campo + '\')">' +
+                        '<div class="partido-slide-overlay"></div>' +
+                        '<div class="partido-slide-info">' +
+                            '<span class="partido-slide-name">' + data[row].nombre_partido + '</span>' +
+                            '<div class="partido-slide-meta">' +
+                                '<span>' + data[row].fecha_partido + '</span>' +
+                                '<span class="separador">·</span>' +
+                                '<span>' + data[row].nombre_competicion + '</span>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>'
+                );
+        }
+
+        new Swiper('.swiper-partidos', {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            autoplay: { delay: 3000, pauseOnMouseEnter: true, disableOnInteraction: false },
+            pagination: { el: '.swiper-pagination-partidos', clickable: true },
+            navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+        });
+    });
+}
+
+function loadCategories() {
+    ajaxPromise('module/home/controller/controller_home.php?op=homePageCategory', 'GET', 'JSON')
+    .then(function(data) {
+        for (let row = 0; row < data.length; row++) {
+            const name = data[row].nombre_competicion;
+            const icon = data[row].img_competicion;
+
+            $('<div></div>')
+                .addClass('category-card')
+                .attr('id', 'cat_' + row)
+                .appendTo('#containerCategories')
+                .html(
+                    '<div class="category-icon">' +
+                        '<img class="logo-icon" src="' + icon + '">' +
+                    '</div>' +
+                    '<span>' + name + '</span>'
+                );
+        }
+        // Sin Swiper aquí
+    });
+}
+
+function loadCities() {
+    ajaxPromise('module/home/controller/controller_home.php?op=homePageCities', 'GET', 'JSON')
+    .then(function(data) {
+        for (let row = 0; row < data.length; row++) {
+            const name = data[row].nombre_ciudad;
+            const icon = data[row].img_ciudad;
+
+            $('<div></div>')
+                .addClass('swiper-slide')
+                .attr('id', 'cit_' + row)
+                .appendTo('#containerCities')
+                .html(
+                    '<div class="category-card city-card" style="background-image:url(\'' + icon + '\');background-size:cover;background-position:center;">' +
+                        '<span class="city-name">' + name + '</span>' +
+                    '</div>'
+                );
+        }
+
+        new Swiper('.swiper-cities', {
+            slidesPerView: 4,
+            spaceBetween: 16,
+            loop: true,                    // vuelve al inicio al llegar al final
+            navigation: { nextEl: '#next-cit', prevEl: '#prev-cit' },
+            grabCursor: true,
+        });
+    });
+}
+
+function loadTeams() {
+    ajaxPromise('module/home/controller/controller_home.php?op=homePageTeams', 'GET', 'JSON')
+    .then(function(data) {
+        for (let row = 0; row < data.length; row++) {
+            const name = data[row].nombre_equipo;
+            const icon = data[row].img_equipo;
+
+            $('<div></div>')
+                .addClass('swiper-slide')
+                .attr('id', 'tea_' + row)
+                .appendTo('#containerTeams')
+                .html(
+                    '<div class="category-card">' +
+                        '<div class="category-icon">' +
+                            '<img class="logo-icon" src="' + icon + '">' +
+                        '</div>' +
+                        '<span>' + name + '</span>' +
+                    '</div>'
+                );
+        }
+
+        new Swiper('.swiper-teams', {
+            slidesPerView: 4,
+            spaceBetween: 16,
+            loop: true,
+            navigation: { nextEl: '#next-tea', prevEl: '#prev-tea' },
+            grabCursor: true,
+        });
+    });
+}
+
+$(document).ready(function () {
+    //console.log('controller_home.js loaded');
+
+    loadCarousel();
+
+    //console.log('1. Document ready');
+    
+    loadCategories();
+    
+    //console.log('2. Después de loadCategories');
+    
+    loadCities();
+    
+    //console.log('3. Después de loadCities');
+    
+    loadTeams();
+    
+    //console.log('4. Después de loadTeams');
+});
